a180710d2df5017254e5bef091d080391e7b5ce1
Vítor Zeni <vsz@certi.org.br>
2019-04-05 18:22:37 -0300

    Formulated the optimization problem

--- main.py
+++ main.py
@@ -1,6 +1,80 @@
 from pulp import *
 from classes import *
 
+"""
+# Creates a list of the Ingredients
+Ingredients = ['CHICKEN', 'BEEF', 'MUTTON', 'RICE', 'WHEAT', 'GEL']
+
+# A dictionary of the costs of each of the Ingredients is created
+costs = {'CHICKEN': 0.013, 
+         'BEEF': 0.008, 
+         'MUTTON': 0.010, 
+         'RICE': 0.002, 
+         'WHEAT': 0.005, 
+         'GEL': 0.001}
+
+# A dictionary of the protein percent in each of the Ingredients is created
+proteinPercent = {'CHICKEN': 0.100, 
+                  'BEEF': 0.200, 
+                  'MUTTON': 0.150, 
+                  'RICE': 0.000, 
+                  'WHEAT': 0.040, 
+                  'GEL': 0.000}
+
+# A dictionary of the fat percent in each of the Ingredients is created
+fatPercent = {'CHICKEN': 0.080, 
+              'BEEF': 0.100, 
+              'MUTTON': 0.110, 
+              'RICE': 0.010, 
+              'WHEAT': 0.010, 
+              'GEL': 0.000}
+
+# A dictionary of the fibre percent in each of the Ingredients is created
+fibrePercent = {'CHICKEN': 0.001, 
+                'BEEF': 0.005, 
+                'MUTTON': 0.003, 
+                'RICE': 0.100, 
+                'WHEAT': 0.150, 
+                'GEL': 0.000}
+
+# A dictionary of the salt percent in each of the Ingredients is created
+saltPercent = {'CHICKEN': 0.002, 
+               'BEEF': 0.005, 
+               'MUTTON': 0.007, 
+               'RICE': 0.002, 
+               'WHEAT': 0.008, 
+               'GEL': 0.000}
+               
+# Create the 'prob' variable to contain the problem data
+prob = LpProblem("The Whiskas Problem", LpMinimize)
+
+# A dictionary called 'ingredient_vars' is created to contain the referenced Variables
+ingredient_vars = LpVariable.dicts("Ingr",Ingredients,0)
+
+# The objective function is added to 'prob' first
+prob += lpSum([costs[i]*ingredient_vars[i] for i in Ingredients]), "Total Cost of Ingredients per can"
+
+# The five constraints are added to 'prob'
+prob += lpSum([ingredient_vars[i] for i in Ingredients]) == 100, "PercentagesSum"
+prob += lpSum([proteinPercent[i] * ingredient_vars[i] for i in Ingredients]) >= 8.0, "ProteinRequirement"
+prob += lpSum([fatPercent[i] * ingredient_vars[i] for i in Ingredients]) >= 6.0, "FatRequirement"
+prob += lpSum([fibrePercent[i] * ingredient_vars[i] for i in Ingredients]) <= 2.0, "FibreRequirement"
+prob += lpSum([saltPercent[i] * ingredient_vars[i] for i in Ingredients]) <= 0.4, "SaltRequirement"
+
+
+prob.solve()
+
+# The status of the solution is printed to the screen
+print("Status:", LpStatus[prob.status])
+
+# Each of the variables is printed with it's resolved optimum value
+for v in prob.variables():
+    print(v.name, "=", v.varValue)
+
+# The optimised objective function value is printed to the screen
+print("Total Cost of Ingredients per can = ", value(prob.objective))
+"""
+
 leg = Resource('Luna')
 leg.setAvailableHours([120, 120, 130, 130, 90, 100, 120, 120, 125, 110, 115, 80])
 leg.addMaxAvailableHourInProject('Copel',150)
@@ -51,22 +125,28 @@ MONTHS = list(range(1,n_months+1))
 COSTS = {}
 for r in resourceList:
 	COSTS[r.name] = r.costInProject
+	
+print('costs=',COSTS)
 
 # Maximum allocation (in projects)
 MAX_ALLOC_PROJ = {}
 for r in resourceList:
 	MAX_ALLOC_PROJ[r.name] = r.maxAvailableHourInProject
+print('max allocation in each project=',MAX_ALLOC_PROJ)
 
 # Maximum allocation (global)
 MAX_ALLOC = {}
 for r in resourceList:
 	MAX_ALLOC[r.name] = r.availableHours
-	
+
+print('max allocation=',MAX_ALLOC)
+
 # Project Monthly Payment
 PROJECTS_MONTHLY_PAYMENTS = {}
 for p in projectList:
 	PROJECTS_MONTHLY_PAYMENTS[p.name] = p.contractMonthlyPayments
-	
+
+print('project monthly payments=',PROJECTS_MONTHLY_PAYMENTS)
 
 # Create the 'prob' variable to contain the problem data
 prob = LpProblem('Resource Allocation Problem', LpMinimize)
@@ -85,16 +165,41 @@ difference_contract_down = pulp.LpVariable.dicts('Difference Payment Down',
 allocations = pulp.LpVariable.dicts('Monthly Rsource Allocations',
 									((r,p,m) for r in RESOURCES for p in PROJECTS for m in MONTHS),
 									lowBound = 0,
+									upBound = 160,
 									cat = 'Integer')
 
 
 # Objective Function
 prob += lpSum([difference_contract_up[p,m] + difference_contract_down[p,m] for p in PROJECTS for m in MONTHS]), 'Difference between contract payment and costs'
 
+# Constraints
+# Difference between contract and costs aux vars
+for m in MONTHS:
+	for p in PROJECTS:
+		prob += lpSum([allocations[r,p,m]*COSTS[r][p] - difference_contract_up[p,m] - PROJECTS_MONTHLY_PAYMENTS[p][m-1] for r in RESOURCES]) <= 0
+		prob += lpSum([allocations[r,p,m]*COSTS[r][p] + difference_contract_down[p,m] - PROJECTS_MONTHLY_PAYMENTS[p][m-1] for r in RESOURCES]) >= 0
+
+# Maximum allocation (global) each month
+for m in MONTHS:
+	prob += lpSum([allocations[r,p,m] - MAX_ALLOC[r][m-1] for r in RESOURCES for p in PROJECTS]) <= 0
+
+# Maximum allocation (in individual projects) each month
+for m in MONTHS:
+	for p in PROJECTS:
+		for r in RESOURCES:
+			prob += allocations[r,p,m] - MAX_ALLOC_PROJ[r][p] <= 0
 
 
 
+prob.solve()
 
+# The status of the solution is printed to the screen
+print("Status:", LpStatus[prob.status])
 
+# Each of the variables is printed with it's resolved optimum value
+for v in prob.variables():
+    print(v.name, "=", v.varValue)
 
+# The optimised objective function value is printed to the screen
+print("Total difference contract/allocation cost = ", value(prob.objective))
 
